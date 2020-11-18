0abff27a98c92225faaa64950d1f97a7ea561739
Patrice Cote <patrice.cote@ia.ca>
2020-11-18 15:01:55 -0500

    Initial creation

---
+++ TodoApi/TodoApi/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+        { }
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>()
+                .Property(b => b.Name)
+                .IsRequired();
+        }
+    }
+}
